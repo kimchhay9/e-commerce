a83b77d4bba9b7b3990d1e207920cbfe0c6db61a
kimchhay9 <soeungkimchhay50@gmail.com>
2024-12-06 10:44:55 -0800

    TP4

---
+++ src/stores/App.vue
@@ -0,0 +1,29 @@
+<script>
+import HomeVue from "./Views/HomeVue.vue";
+import { RouterView } from "vue-router";
+
+export default {
+  components: {
+    HomeVue,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  border: 2px solid;
+  box-sizing: border-box;
+
+  color: black;
+}
+
+html {
+  background-color: rgb(255, 255, 255);
+}
+</style>
